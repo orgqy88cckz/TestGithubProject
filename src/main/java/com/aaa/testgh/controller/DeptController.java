e4b81767e24eb064026357d1bba9aaa3e1befd4f
chengfangying <3213658597@qq.com>
2018-11-29 09:30:41 +0800

    Initial commit

---
+++ src/main/java/com/aaa/testgh/controller/DeptController.java
@@ -0,0 +1,13 @@
+package com.aaa.testgh.controller;
+
+/**
+ * className:DeptController
+ * discription:
+ * author:Cheng Fangying
+ * createTime:2018-11-29 09:05
+ */
+public class DeptController {
+    public static void main(String[] args) {
+        System.out.println("测试github提交1111111");
+    }
+}
