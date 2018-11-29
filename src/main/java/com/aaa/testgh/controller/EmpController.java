cfc5a25de9b3275fc02834752ce78de6b6c24ce6
chengfangying <3213658597@qq.com>
2018-11-29 09:36:21 +0800

    新加了一个EmpController

---
+++ src/main/java/com/aaa/testgh/controller/EmpController.java
@@ -0,0 +1,11 @@
+package com.aaa.testgh.controller;
+
+/**
+ * className:EmpController
+ * discription:
+ * author:Cheng Fangying
+ * createTime:2018-11-29 09:32
+ */
+public class EmpController
+{
+}
